1390edb4521d39ecfde9e0832a8cea227a0e0a8b
Htripathi07 <128625047+Htripathi07@users.noreply.github.com>
2023-11-27 23:16:58 +0530

    frontend

--- src/data/ProjectData.js
+++ src/data/ProjectData.js
@@ -9,6 +9,15 @@ export const ProjectList = [
     demo_url: "https://myntra8.netlify.app/",
   },
   {
+    img: "https://drive.google.com/uc?id=1rkqlhl3-ZG5Qt8aeHH_rm-ojctXgOxbg",
+    title: "Foodie-App",
+    description:
+      "Find and order from local restaurants to satisfy your cravings with our easy-to-use foodie app. Get started today!",
+    tech_stack: "React, CSS, Javascript, ES6 etc",
+    github_url: "https://github.com/Htripathi07/Foodie-Ordering-Website",
+    demo_url: "https://foodie-app-7.netlify.app",
+  },
+  {
     img: "https://drive.google.com/uc?id=1GpZ6od-tjIJf_0X401aIQPCqdrk63Nx9",
     title: "Mwendo Africa : Rental website",
     description:
@@ -18,6 +27,15 @@ export const ProjectList = [
     demo_url: "https://mwendu-cw2.netlify.app/",
   },
   {
+    img: "https://drive.google.com/uc?id=1O-7qFJBCWqDFQlNFLELswyGfcepdew81",
+    title: "Tic-Tac-toe Game",
+    description:    
+"Experience the classic Tic Tac Toe game with a modern twist in our React-based interactive web app.",
+    tech_stack: "React, CSS, Javascript, ES6 etc",
+    github_url: "https://github.com/Htripathi07/TicTacToe-Game",
+    demo_url: "https://tic1-tac-toe.netlify.app/",
+  },
+  {
     img: "https://drive.google.com/uc?id=1Gqe-7iIjXAjybgJevxBt13QAB1b7kbTY",
     title: "Buddha Ventures:One page site",
     description: `Welcome to Buddha Ventures: Rediscover the essence of Buddhism and join us in creating real-world impact. Explore our mission to align modern practices with Buddha's vision.`,
@@ -25,6 +43,7 @@ export const ProjectList = [
     github_url: "https://github.com/Htripathi07/team8_Myntra_clone_project",
     demo_url: "https://projbuddh.netlify.app/",
   },
+
   {
     img: "https://drive.google.com/uc?id=1GrIQlHOcvvo6PnlnBwx5zX46YNTtT6WA",
     title: "TODO-App",
@@ -40,31 +59,31 @@ export const ProjectList = [
 
 export const stackList = [
   {
-    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/html5.svg",
+    img: "https://drive.google.com/uc?id=1iwW4VTj8O2dU8LS0qZr_XGoJ5miK0It4",
     name: "HTML",
   },
   {
-    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/css3.svg",
+    img: "https://drive.google.com/uc?id=16WKsmXVA7qULrx9y_fnNr49gY5ccML3n",
     name: "CSS",
   },
   {
-    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/javascript.svg",
+    img: "https://drive.google.com/uc?id=1lNWK-A16xbi0GS_xGw6SXF5tE7mqxSsM",
     name: "JavaScript",
   },
   {
-    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/react.svg",
+    img: "https://drive.google.com/uc?id=1Kgv8uqcmeKHTml_V3hTypDbmo-RjePOw",
     name: "ReactJS",
   },
   {
-    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/redux/redux-original.svg",
+    img: "https://drive.google.com/uc?id=1x3418n4cCiX_0ug0T62bRaeAzVS5eHSq",
     name: "Redux",
   },
   {
-    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg",
+    img: "https://drive.google.com/uc?id=1aUIRLGBH8yTaUXf1wfmAAn2dKaWrkQMq",
     name: "NodeJS",
   },
   {
-    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+    img: "https://drive.google.com/uc?id=1gHHWqn_ALqH50eKVcUWJ_pcTPon1bKaY",
     name: "MongoDB",
   },
   {
@@ -72,7 +91,7 @@ export const stackList = [
     name: "Express",
   },
   {
-    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg",
+    img: "https://drive.google.com/uc?id=1eM0jTIKxPlxvTZyeMY57pWzcvc_jgKPi",
     name: "TypeScript",
   },
   {
@@ -80,7 +99,7 @@ export const stackList = [
     name: "Chakra Ui",
   },
   {
-    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/git.svg",
+    img: "https://drive.google.com/uc?id=1WS19WsaG9ATxxPkBv2xBkVfPQMn4r_VT",
     name: "Git",
   },
   {
